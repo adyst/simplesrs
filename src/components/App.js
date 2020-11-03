38f614538d8a41f9f9cc7fb8d9ad39b3297a5fcb
Araceli Yanez <adyst@users.noreply.github.com>
2020-11-03 08:18:40 -0800

    Create bare bones React setup.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <section>
+      App!!
+    </section>
+  )
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/dist'),
+    index: 'index.html'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
+

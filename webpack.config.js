b7fe9d0c02ea25a455ed6eb571071ee2414e41c7
Araceli Yanez <adyst@users.noreply.github.com>
2020-11-04 09:21:38 -0800

    Added TypeScript.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '/dist')
@@ -14,6 +14,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
       }
     ]
   },

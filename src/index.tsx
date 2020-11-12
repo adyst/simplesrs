a2fbd09866fe7a95e4c7fe7d7df1f8d4057c3a29
Araceli Yanez <adyst@users.noreply.github.com>
2020-11-12 09:16:50 -0800

    Started adding redux.

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import { BrowserRouter as Router } from 'react-router-dom'; 
+import store from './lib/store/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <Router>
-    <App/>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App/>
+    </Router>
+  </Provider>,
   document.getElementById('app')
 );
